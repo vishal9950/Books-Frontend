a2058de173d86327ccb6718d76840bf28028a3d6
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-02-21 10:43:58 +0530

    Add Side Nav Bar component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import SideNav from '../SideNav/SideNav';
 
 const App = () => (
-  <div className="App">Hello</div>
+  <div className="App"><SideNav /></div>
 );
 
 export default App;
---
+++ src/Components/SideNav/SideNav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './SideNav.css';
+
+class SideNav extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      logoText: 'Bs',
+    };
+  }
+
+  render() {
+    return (
+      <div className="SideNav-bar">
+        <ul className="SideNav-list">
+          <li id="SideNav-logo">{this.state.logoText}</li>
+          <li id="SideNav-blank" />
+          <li><i className="material-icons">refresh</i></li>
+          <li id="SideNav-blank2" />
+          <li><i className="material-icons">settings</i></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default SideNav;
+
